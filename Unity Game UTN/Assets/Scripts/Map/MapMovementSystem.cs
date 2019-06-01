0b47a2550a856629e30e6db86cc49165af0aa268
Ian Acosta <45457585+IanAcosta7@users.noreply.github.com>
2019-06-01 15:25:10 -0300

    Map Movement System
    - Created the branch.
    - Added the MMS Script for the map.
    - Added a simple version of the map.

---
+++ Unity Game UTN/Assets/Scripts/Map/MapMovementSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMovementSystem : MonoBehaviour
+{
+
+    public float speed;
+
+    private Transform objectTransform;
+    private Vector3 initialPos;
+
+    void Start()
+    {
+        objectTransform = GetComponent<Transform>();
+        initialPos = objectTransform.position;
+    }
+
+    void Update()
+    {
+        objectTransform.Translate(new Vector3(0f, 0f, -1f) * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("border"))
+        {
+            objectTransform.position = initialPos;
+        }
+    }
+}
